00a83d058e222810a80866ab405f4f9ce74f83cd
David Masover <ninja@slaphack.com>
2010-05-24 00:28:38 -0500

    Forward integer and decimal seems to work. Reverse integer works.

--- lexidecimal.rb
+++ lexidecimal.rb
@@ -8,13 +8,42 @@ module Lexidecimal
     string.each_char.map{|d| 9-d.to_i}.join
   end
   
-  def self.num_to_string num
+  def self.integer_to_string num, sign=nil, negative=nil
+    return '0' if num == 0
+    
+    if sign.nil?
+      if num < 0
+        sign = MINUS
+        negative = true
+        num = -num
+      else
+        sign = PLUS
+        negative = false
+      end
+    end
+    
+    string = num.to_s
+    string = self.invert string if negative
+    
+    length = string.length
+    if length > 1
+      "#{sign}#{self.integer_to_string length, sign, negative}#{string}"
+    else
+      "#{sign}#{string}"
+    end
+  end
+  
+  def self.decimal_to_string num
     unless num.kind_of? BigDecimal
       num = BigDecimal.new(num.to_s)
     end
     
     sign, digits, base, exponent = num.split
-    if sign < 1
+    
+    return '0' if digits == '0'
+    
+    negative = (sign < 1)
+    if negative
       digits = self.invert digits
       sign = MINUS
       antisign = PLUS
@@ -23,14 +52,33 @@ module Lexidecimal
       antisign = MINUS
     end
     
-    if exponent < 0
-      exp_sign = MINUS * (Math.log10(-exponent).floor+1)
-      exponent = self.invert exponent.to_s
-    else
-      exp_sign = PLUS * (Math.log10(exponent).floor+1)
-      exponent = exponent.to_s
+    "#{sign}#{self.integer_to_string(negative ? -exponent : exponent)}#{digits}#{antisign}"
+  end
+  
+  def self.string_to_int string
+    self.enum_to_int string.each_char
+  end
+  
+  def self.enum_to_int enum
+    sign = enum.next
+    return 0 if sign == '0'
+    count = 0
+    while (cur = enum.next) == sign
+      count += 1
     end
-    
-    "#{sign}#{exp_sign}#{exponent}#{digits}#{antisign}"
+    negative = (sign == MINUS)
+    length = cur.to_i
+    length = 9 - length if negative
+    count.times do
+      new_length = 0
+      length.times do
+        new_length *= 10
+        digit = enum.next.to_i
+        digit = 9 - digit if negative
+        new_length += digit
+      end
+      length = new_length
+    end
+    negative ? -length : length
   end
 end
